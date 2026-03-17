a6016663811e9b00001acdaecea67a01c76b1b8f
robertorua-droid <roberto.rua@gmail.com>
2026-03-17 20:05:02 +0100

    Update config.js

--- src/core/config.js
+++ src/core/config.js
@@ -5,7 +5,7 @@ export const config = {
   // Email che devono avere accesso completo (ruolo Supervisor).
   // Inserisci qui l'email del docente (minuscole), es:
   // 'docente@scuola.it'
-  SUPERVISOR_EMAILS: [roberto.rua@gmail.com],
+  SUPERVISOR_EMAILS: ['roberto.rua@gmail.com'],
 
   // Ruolo di default per chi si registra
   DEFAULT_ROLE: 'User'
